95a288fea5854d6d0e6eab0a80f5588785dd4f55
Ted Kulp <ted@cmsmadesimple.org>
2011-08-15 07:55:02 -0400

    Fix indenting

--- plugins/post_filters.rb
+++ plugins/post_filters.rb
@@ -1,17 +1,17 @@
 require 'pp'
 module Jekyll
   class PostFilter < Plugin
-	  #Called before post is sent to the converter. Allows
-	  #you to modify the post object before the converter
-	  #does it's thing
-	  def pre_render(post)
-	  end
+    #Called before post is sent to the converter. Allows
+    #you to modify the post object before the converter
+    #does it's thing
+    def pre_render(post)
+    end
 
-	  #Called after the post is rendered with the converter.
-	  #Use the post object to modify it's contents before the
-	  #post is inserted into the template.
-	  def post_render(post)
-	  end
+    #Called after the post is rendered with the converter.
+    #Use the post object to modify it's contents before the
+    #post is inserted into the template.
+    def post_render(post)
+    end
 
     #Called after the post is written to the disk.
     #Use the post object to read it's contents to do something
