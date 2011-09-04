74ab8b2ee7b3358a432a05a9de4559dff59f3812
Ted Kulp <ted@cmsmadesimple.org>
2011-09-04 08:41:44 -0400

    Make post_filters work on Pages and Posts

--- plugins/post_filters.rb
+++ plugins/post_filters.rb
@@ -61,44 +61,26 @@ module Jekyll
     # Returns nothing
     def write(dest)
       old_write(dest)
-
-      if self.site.post_filters
-        self.site.post_filters.each do |filter|
-          filter.post_write(self)
-        end
-      end
+      post_write if respond_to?(:post_write)
     end
+  end
 
-    # Call the pre_render methods on all of the loaded
-    # post_filter plugins.
+  # Monkey patch for the Jekyll Page class. For the original class,
+  # see: https://github.com/mojombo/jekyll/blob/master/lib/jekyll/page.rb
+  class Page
+
+    # Copy the #write method to #old_write, so we can redefine #write
+    # method.
+    alias_method :old_write, :write
+
+    # Write the generated post file to the destination directory. It
+    # then calls any post_write methods that may exist.
+    #   +dest+ is the String path to the destination dir
     #
     # Returns nothing
-    def pre_render
-      if self.site.post_filters
-        self.site.post_filters.each do |filter|
-          filter.pre_render(self)
-        end
-      else
-        self.site.load_post_filters
-      end
-    end
-
-    # Call the post_render methods on all of the loaded
-    # post_filter plugins.
-    #
-    # Returns nothing
-    def post_render
-      if self.site.post_filters
-        self.site.post_filters.each do |filter|
-          filter.post_render(self)
-        end
-      end
-    end
-
-    # Returns the full url of the post, including the
-    # configured url
-    def full_url
-      self.site.config['url'] + self.url
+    def write(dest)
+      old_write(dest)
+      post_write if respond_to?(:post_write)
     end
   end
 
@@ -106,12 +88,62 @@ module Jekyll
   # see: https://github.com/mojombo/jekyll/blob/master/lib/jekyll/convertible.rb
   module Convertible
 
+    def is_post?
+      self.class.to_s == 'Jekyll::Post'
+    end
+
+    def is_page?
+      self.class.to_s == 'Jekyll::Page'
+    end
+
+    def is_filterable?
+      is_post? or is_page?
+    end
+
+    # Call the #pre_render methods on all of the loaded
+    # post_filter plugins.
+    #
+    # Returns nothing
+    def pre_render
+      self.site.load_post_filters unless self.site.post_filters
+
+      if self.site.post_filters and is_filterable?
+        self.site.post_filters.each do |filter|
+          filter.pre_render(self)
+        end
+      end
+    end
+
+    # Call the #post_render methods on all of the loaded
+    # post_filter plugins.
+    #
+    # Returns nothing
+    def post_render
+      if self.site.post_filters and is_filterable?
+        self.site.post_filters.each do |filter|
+          filter.post_render(self)
+        end
+      end
+    end
+
+    # Call the #post_write methods on all of the loaded
+    # post_filter plugins.
+    #
+    # Returns nothing
+    def post_write
+      if self.site.post_filters and is_filterable?
+        self.site.post_filters.each do |filter|
+          filter.post_write(self)
+        end
+      end
+    end
+
     # Copy the #transform method to #old_transform, so we can
     # redefine #transform method.
     alias_method :old_transform, :transform
 
     # Transform the contents based on the content type. Then calls the
-    # post_render method if it exists
+    # #post_render method if it exists
     #
     # Returns nothing.
     def transform
@@ -133,6 +165,12 @@ module Jekyll
     def do_layout(payload, layouts)
       pre_render if respond_to?(:pre_render)
       old_do_layout(payload, layouts)
+    end
+
+    # Returns the full url of the post, including the
+    # configured url
+    def full_url
+      self.site.config['url'] + self.url
     end
   end
 end
--- plugins/post_twitter.rb
+++ plugins/post_twitter.rb
@@ -13,31 +13,33 @@ module Jekyll
     require 'twitter'
 
     def post_write(post)
-      should_blast?(post) do
-        c = post.site.config
-        if c.has_key?('twitter') and c['twitter'].has_key?('consumer_key') and c['twitter'].has_key?('consumer_secret') and c['twitter'].has_key?('oauth_token') and c['twitter'].has_key?('oauth_token_secret')
-          begin
-            Twitter.configure do |config|
-              config.consumer_key = c['twitter']['consumer_key']
-              config.consumer_secret = c['twitter']['consumer_secret']
-              config.oauth_token = c['twitter']['oauth_token']
-              config.oauth_token_secret = c['twitter']['oauth_token_secret']
+      if post.is_post?
+        should_blast?(post) do
+          c = post.site.config
+          if c.has_key?('twitter') and c['twitter'].has_key?('consumer_key') and c['twitter'].has_key?('consumer_secret') and c['twitter'].has_key?('oauth_token') and c['twitter'].has_key?('oauth_token_secret')
+            begin
+              Twitter.configure do |config|
+                config.consumer_key = c['twitter']['consumer_key']
+                config.consumer_secret = c['twitter']['consumer_secret']
+                config.oauth_token = c['twitter']['oauth_token']
+                config.oauth_token_secret = c['twitter']['oauth_token_secret']
+              end
+
+              #Grab the bitly URL
+              url = self.shortened_url(post)
+
+              #Make a title string
+              title = 'Post: ' + post.data['title']
+
+              #Shrink it so it's twitter friendly
+              msg = truncate(title, 140 - url.size - 3) + " - " + url
+
+              #Blast it to the world
+              client = Twitter::Client.new
+              client.update(msg)
+            rescue Twitter::Forbidden
+              puts "Twitter updated failed: continuing..."
             end
-
-            #Grab the bitly URL
-            url = self.shortened_url(post)
-
-            #Make a title string
-            title = 'Post: ' + post.data['title']
-
-            #Shrink it so it's twitter friendly
-            msg = truncate(title, 140 - url.size - 3) + " - " + url
-
-            #Blast it to the world
-            client = Twitter::Client.new
-            client.update(msg)
-          rescue Twitter::Forbidden
-            puts "Twitter updated failed: continuing..."
           end
         end
       end
